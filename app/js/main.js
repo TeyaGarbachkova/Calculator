617e08e2eb1d5a0c655a68ea69b5c9d3163af1da
TeyaGarbachkova <doreto9@abv.bg>
2018-04-07 14:47:28 +0300

    add functionality

--- app/js/main.js
+++ app/js/main.js
@@ -4,35 +4,49 @@
     // Variables
     var screen = document.querySelector('.display-num'), // Display numbers
         screenResult = document.querySelector('.display'),
+        warming = document.querySelector('.warming'),
         equals = document.querySelector('#equals'),
         elem = document.querySelectorAll('.num'), // List of numbers
         elem1 = document.querySelectorAll('.ops'), // List of operators
         clear = document.querySelector('.clear'),
         delLast = document.querySelector('.del-last'),
-        output,
+        zero = document.querySelector('.num-null'),
+        period = document.querySelector('.period'),
+        output = '',
         result, //Result
         operator,
-        limit,
-        zero = document.querySelector('.num-null'),
-        period = document.querySelector('.period');
+        limit;
 
 
-    for(var i = 0, len = elem.length; i < len; i++ ) {
-        elem[i].onclick = function() {
-            var num = this.getAttribute("data-num");
-            output = num;
-            //output = screen.innerHTML + num;
-            //console.log(output);
-            result = screen.innerHTML += num;
-            screenResult.innerHTML = output;
-            limit = output.length;
-            if(limit > 16 ) {
-                alert("Sorry no more input is allowed");
+    var setNum = function() {
+        var num = this.getAttribute("data-num");
+        output += num;
+        result = screen.innerHTML += num;
+        screenResult.innerHTML = output;
+
+        limit = result.length;
+        if(limit > 30 ) {
+            warming.innerHTML = "Sorry no more input is allowed";
+            for (var i = 0, len = elem.length; i < len; i++ ) {
+                elem[i].setAttribute("disabled", "true");
             }
-        };
-    }
+        }
+    };
 
-    zero.onclick = function() {
+    var moveNum = function() {
+        operator = this.getAttribute("data-ops");
+        if(screen.innerHTML === "") {
+            screen.innerHTML = screen.innerHTML.concat("");
+        }
+        else if(result) {
+            result = result.concat(operator);
+            screen.innerHTML = result;
+            output = "";
+            screenResult.innerHTML = output;
+        }
+    };
+
+    var zeroNum = function() {
         zero = this.getAttribute("data-num");
         if(screen.innerHTML === "") {
             output = screen.innerHTML = zero;
@@ -42,7 +56,7 @@
         }
     };
 
-    period.onclick = function() {
+    var periodNum = function() {
         period = this.getAttribute("data-num");
         if(screen.innerHTML === "") {
             output = screen.innerHTML = screen.innerHTML.concat("0.");
@@ -52,19 +66,38 @@
         }
     };
 
-    equals.onclick = function() {
+    var equalsNum = function() {
         if(screen.innerHTML === result) {
             screenResult.innerHTML = eval(result);
+            screen.innerHTML = result + "=" + eval(result);
         }
         else {
             screenResult.innerHTML = "";
         }
     };
 
-    clear.onclick = function() {
+    var clearNums = function() {
+        output = "";
+        result = "";
         screen.innerHTML = "";
         screenResult.innerHTML = "";
     };
+
+    for(var i = 0, len = elem.length; i < len; i++ ) {
+        elem[i].onclick = setNum;
+    }
+
+    for(var i = 0, len1 = elem1.length; i < len1; i++ ) {
+        elem1[i].onclick = moveNum;
+    }
+
+    zero.onclick = zeroNum;
+
+    period.onclick = periodNum;
+
+    equals.onclick = equalsNum;
+
+    clear.onclick = clearNums;
 
     delLast.onclick = function() {
         if (screenResult.innerHTML !== "") {
@@ -72,20 +105,4 @@
         }
     };
 
-    for(var i = 0, len1 = elem1.length; i < len1; i++ ) {
-        elem1[i].onclick = function() {
-            operator = this.getAttribute("data-ops");
-            this.setAttribute("clicked", "true");
-            if(screen.innerHTML === "") {
-                screen.innerHTML = screen.innerHTML.concat("");
-            }
-            else if(result) {
-                result = result.concat(operator);
-                screen.innerHTML = result;
-                screenResult.innerHTML = output;
-                console.log(output);
-            }
-
-        };
-    }
 }());
