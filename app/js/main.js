0a033610bdd79029f150eff230b291dda04ae0b1
TeyaGarbachkova <doreto9@abv.bg>
2018-04-10 00:35:59 +0300

    correct js html css files

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,15 @@
 (function() {
-    "use strict"
+    'use strict'
 
     // Variables
     var screen = document.querySelector('.display-num'), // Display numbers
-        screenResult = document.querySelector('.display'),
+        screenResult = document.querySelector('.display'), // Display result
         warming = document.querySelector('.warming'),
-        equals = document.querySelector('#equals'),
+        equals = document.querySelector('.equals'),
         elem = document.querySelectorAll('.num'), // List of numbers
-        elem1 = document.querySelectorAll('.ops'), // List of operators
+        ops = document.querySelectorAll('.ops'), // List of operators
         clear = document.querySelector('.clear'),
         delLast = document.querySelector('.del-last'),
-        zero = document.querySelector('.num-null'),
         period = document.querySelector('.period'),
         output = '',
         result, //Result
@@ -19,79 +18,71 @@
 
 
     var setNum = function() {
-        var num = this.getAttribute("data-num");
+        var num = this.getAttribute('data-num');
         output += num;
         result = screen.innerHTML += num;
         screenResult.innerHTML = output;
 
         limit = result.length;
         if(limit > 30 ) {
-            warming.innerHTML = "Sorry no more input is allowed";
+            warming.innerHTML = 'Sorry no more input is allowed';
             for (var i = 0, len = elem.length; i < len; i++ ) {
-                elem[i].setAttribute("disabled", "true");
+                elem[i].setAttribute('disabled', 'true');
             }
         }
     };
 
     var moveNum = function() {
-        operator = this.getAttribute("data-ops");
-        if(screen.innerHTML === "") {
-            screen.innerHTML = screen.innerHTML.concat("");
+        operator = this.getAttribute('data-ops');
+
+        if(screen.innerHTML === '') {
+            screen.innerHTML = screen.innerHTML.concat('');
         }
         else if(result) {
             result = result.concat(operator);
             screen.innerHTML = result;
-            output = "";
+            output = '';
             screenResult.innerHTML = output;
         }
     };
 
-    var zeroNum = function() {
-        zero = this.getAttribute("data-num");
-        if(screen.innerHTML === "") {
-            output = screen.innerHTML = zero;
-        }
-        else if(screen.innerHTML === output) {
-            output = screen.innerHTML += zero;
-        }
-    };
-
     var periodNum = function() {
-        period = this.getAttribute("data-num");
-        if(screen.innerHTML === "") {
-            output = screen.innerHTML = screen.innerHTML.concat("0.");
+        period = this.getAttribute('data-num');
+        if(screen.innerHTML === '' && screenResult.innerHTML === '') {
+            result = screen.innerHTML = screen.innerHTML.concat('0.');
+            output = screenResult.innerHTML = screenResult.innerHTML.concat('0.');
         }
-        else if(screen.innerHTML === output) {
-            screen.innerHTML = screen.innerHTML.concat(".");
+        else if(screenResult.innerHTML === output && screen.innerHTML === result) {
+            output = output + period;
+            screen.innerHTML = screen.innerHTML.concat('.');
+            screenResult.innerHTML = screenResult.innerHTML.concat('.');
         }
     };
 
     var equalsNum = function() {
         if(screen.innerHTML === result) {
             screenResult.innerHTML = eval(result);
-            screen.innerHTML = result + "=" + eval(result);
+            screen.innerHTML = result + '=' + eval(result);
         }
         else {
-            screenResult.innerHTML = "";
+            screenResult.innerHTML = '';
         }
     };
 
     var clearNums = function() {
-        output = "";
-        result = "";
-        screen.innerHTML = "";
-        screenResult.innerHTML = "";
+        output = '';
+        result = '';
+        screen.innerHTML = '';
+        screenResult.innerHTML = '';
     };
 
     for(var i = 0, len = elem.length; i < len; i++ ) {
         elem[i].onclick = setNum;
     }
 
-    for(var i = 0, len1 = elem1.length; i < len1; i++ ) {
-        elem1[i].onclick = moveNum;
+    for(var i = 0, len1 = ops.length; i < len1; i++ ) {
+        ops[i].onclick = moveNum;
     }
-
-    zero.onclick = zeroNum;
 
     period.onclick = periodNum;
 
@@ -100,9 +91,11 @@
     clear.onclick = clearNums;
 
     delLast.onclick = function() {
-        if (screenResult.innerHTML !== "") {
-            screenResult.innerHTML = output.slice(0, -1);
+        if (screenResult.innerHTML !== '') {
+            output = output.slice(0, -1);
+            result = result.slice(0, -1);
+            screenResult.innerHTML = output;
+            screen.innerHTML = result;
         }
     };
-
 }());
